6e699e029baf1ea8b79ff541f71ffb7556f4055c
Zainab273 <rashidzainab990@gmail.com>
2025-04-16 00:03:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ["Web developer", "Frontend developer", "Fullstack developer", "Software engineer"],
+    typeSpeed: 50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+  });
